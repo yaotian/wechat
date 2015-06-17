3bb56a155bd23e12a2d3b55551e0d7b3587146b0
yaotian <yaotian@gmail.com>
2015-06-17 23:12:40 +0800

    CDTA格式

--- entry/response.go
+++ entry/response.go
@@ -2,108 +2,115 @@ package entry
 
 import (
 	"encoding/xml"
-	"time"
 	"errors"
+	"time"
 )
 
-
-type Response struct{
-	ToUserName string 
-	FromUserName string 
-	MsgType string
-	CreateTime time.Duration 
+type CDATAText struct {
+	Text string `xml:",innerxml"`
 }
 
-type TextResponse struct{
+func value2CDATA(v string) CDATAText {
+	return CDATAText{"<![CDATA[" + v + "]]>"}
+}
+
+type Response struct {
+	ToUserName   CDATAText
+	FromUserName CDATAText
+	MsgType      CDATAText
+	CreateTime   time.Duration
+}
+
+type TextResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
-	Content string	
+	Content CDATAText
 }
 
-type ImageResponse struct{
+type ImageResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
-	MediaId string	`xml:"Image>MediaId"`
+	MediaId string `xml:"Image>MediaId"`
 }
 
-type VoiceResponse struct{
+type VoiceResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
-	MediaId string	`xml:"Voice>MediaId"`	
+	MediaId string `xml:"Voice>MediaId"`
 }
 
-type VideoResponse struct{
+type VideoResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
-	MediaId string	`xml:"Video>MediaId"`	
-	Title	string  `xml:"Video>Title"`
-	Description string	`xml:"Video>Description"`
+	MediaId     string `xml:"Video>MediaId"`
+	Title       string `xml:"Video>Title"`
+	Description string `xml:"Video>Description"`
 }
 
-type MusicResponse struct{
+type MusicResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
-	Title	string  `xml:"Music>Title"`
-	Description string	`xml:"Music>Description"`
-	MusicUrl string `xml:"Music>MusicUrl"`
-	HQMusicUrl string `xml:"Music>HQMusicUrl"`
-	ThumbMediaId string `xml:"Music>ThumbMediaId"`	
+	Title        string `xml:"Music>Title"`
+	Description  string `xml:"Music>Description"`
+	MusicUrl     string `xml:"Music>MusicUrl"`
+	HQMusicUrl   string `xml:"Music>HQMusicUrl"`
+	ThumbMediaId string `xml:"Music>ThumbMediaId"`
 }
 
-type NewsResponse struct{
+type NewsResponse struct {
 	XMLName xml.Name `xml:"xml"`
 	Response
 	ArticleCount int
-	News Articles	`xml:"Articles"`
+	News         Articles `xml:"Articles"`
 }
 
 func NewTextResponse(from string, to string, content string) *TextResponse {
 	text := new(TextResponse)
-	text.FromUserName = from
-	text.ToUserName = to
-	text.MsgType = "text"
-	text.Content = content
+	text.FromUserName = value2CDATA(from)
+	text.ToUserName = value2CDATA(to)
+	text.MsgType = value2CDATA("text")
+	text.Content = value2CDATA(content)
 	text.CreateTime = time.Duration(time.Now().Unix())
 	return text
 }
 
 func NewImageResponse(from string, to string, media string) *ImageResponse {
 	image := new(ImageResponse)
-	image.FromUserName = from
-	image.ToUserName = to
+	image.FromUserName = value2CDATA(from)
+	image.ToUserName = value2CDATA(to)
 	image.MediaId = media
-	image.MsgType = "image"
+	image.MsgType = value2CDATA("image")
 	image.CreateTime = time.Duration(time.Now().Unix())
 	return image
 }
 
 func NewVoiceResponse(from string, to string, media string) *VoiceResponse {
 	voice := new(VoiceResponse)
-	voice.FromUserName = from
-	voice.ToUserName = to
+	voice.FromUserName = value2CDATA(from)
+	voice.ToUserName = value2CDATA(to)
 	voice.MediaId = media
-	voice.MsgType = "voice"
+	voice.MsgType = value2CDATA("voice")
 	voice.CreateTime = time.Duration(time.Now().Unix())
 	return voice
 }
 
 func NewVideoResponse(from string, to string, media string, title string, description string) *VideoResponse {
 	video := new(VideoResponse)
-	video.FromUserName = from
-	video.ToUserName = to
+	video.FromUserName = value2CDATA(from)
+	video.ToUserName = value2CDATA(to)
 	video.MediaId = media
 	video.Title = title
 	video.Description = description
-	video.MsgType = "video"
+	video.MsgType = value2CDATA("video")
 	video.CreateTime = time.Duration(time.Now().Unix())
 	return video
 }
 
-func NewMusicResponse(from, to, title, description, musicurl,hqmusicurl,thumb string) *MusicResponse {
+func NewMusicResponse(from, to, title, description, musicurl, hqmusicurl, thumb string) *MusicResponse {
 	music := new(MusicResponse)
-	music.FromUserName = from
-	music.ToUserName = to
-	music.MsgType = "music"
+	music.FromUserName = value2CDATA(from)
+	music.ToUserName = value2CDATA(to)
+	music.MsgType = value2CDATA("music")
 	music.Title = title
 	music.Description = description
 	music.MusicUrl = musicurl
@@ -114,14 +121,14 @@ func NewMusicResponse(from, to, title, description, musicurl,hqmusicurl,thumb st
 
 func NewNewsResponse(from string, to string) *NewsResponse {
 	news := new(NewsResponse)
-	news.FromUserName = from
-	news.ToUserName = to
-	news.MsgType = "news"
+	news.FromUserName = value2CDATA(from)
+	news.ToUserName = value2CDATA(to)
+	news.MsgType = value2CDATA("news")
 	news.ArticleCount = 0
 	return news
 }
 
-func (news *NewsResponse) Append(article *Article) error{
+func (news *NewsResponse) Append(article *Article) error {
 	if len(news.News.Item) >= 10 {
 		return errors.New("entry NewsResponse: news response append exceed 10 articles already.")
 	}
